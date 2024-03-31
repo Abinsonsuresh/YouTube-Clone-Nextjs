9500fa733cd61ea0557c613b2f1f6350dfbbf935
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-01 01:30:06 +0530

    v details fetched

--- src/Pages/VideoDetails/VDetails.jsx
+++ src/Pages/VideoDetails/VDetails.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const VDetails = () => {
   return (
     <div>VDetails</div>
--- src/app/Video/[id]/page.js
+++ src/app/Video/[id]/page.js
@@ -1,8 +1,25 @@
 'use client'
+import VDetails from "@/Pages/VideoDetails/VDetails"
+import { FetchAPIData } from "@/utils/ApiFetch";
+import { useEffect, useState } from "react"
 
-const page = () => {
+const page = ({params}) => {
+
+    const VId = params.id;
+    // setVideoDetail(data.items[0])
+    const [details, setDetails] = useState(null)
+    useEffect(()=>{
+        FetchAPIData(`videos?part=snippet,statistics&id=${VId}`)
+        .then((data) => 
+        setDetails(data.items[0])
+        )
+  
+    },[VId])
+console.log("D",details)
   return (
-    <div>page</div>
+    <div>
+        <VDetails Details={details}/>
+    </div>
   )
 }
 
