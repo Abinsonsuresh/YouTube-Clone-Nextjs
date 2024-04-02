1846417a421af3e443cc6b138f3d9105f0e4c585
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-03 02:56:43 +0530

    react player adn video details added

--- src/Pages/VideoDetails/VideoData.jsx
+++ src/Pages/VideoDetails/VideoData.jsx
@@ -1,11 +1,20 @@
+'use client'
+import Navbar from '@/components/Navbar';
 import { FetchAPIData } from '@/utils/ApiFetch';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
-import VDetails from './VDetails';
+import ReactPlayer from 'react-player';
 
 const VideoData = ({id}) => {
 
     const VId = id;
+    const router = useRouter()
     // setVideoDetail(data.items[0])
+    if(VId === null  || VId === "undefined")
+    {
+      router.push('/')
+    }
+    console.log(VId)
     const [details, setDetails] = useState(null)
     useEffect(()=>{
         FetchAPIData(`videos?part=snippet,statistics&id=${VId}`)
@@ -17,7 +26,23 @@ const VideoData = ({id}) => {
 console.log("D",details)
   return (
     <div>
-        <VDetails/>
+      <Navbar/>
+      <ReactPlayer url={`https://www.youtube.com/watch?v=${VId}`} className="react-player " height={400} controls />
+
+      <div className='flex flex-col'> 
+        <p>{details?.snippet?.title}</p>
+        <p>{details?.snippet?.channelTitle}</p>
+        <p>{details?.snippet?.description}</p>
+        <p>{details?.statistics?.viewCount}</p>
+        <p>{details?.statistics?.likeCount}</p>
+
+
+
+
+
+      </div>
+      
+ 
     </div>
   )
 }
