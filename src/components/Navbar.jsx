2d8f02bc503a65b0110779295e611a7d3f8e6faf
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-06 01:44:26 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,23 +26,23 @@ const Navbar = () => {
                     <div className="flex items-center gap-5">
                         <form className='flex items-center relative' action="">
                             <input type="text" placeholder='Search' className='hidden md:block p-2 px-4 rounded-full bg-transparent outline-none border border-[#5c5c5c83] lg:min-w-[400px] max-w-[400px]' />
-                            <div className='md:bg-[#262626] p-3 absolute right-0 rounded-r-full'>
+                            <div className='md:bg-[#262626] hidden md:flex p-3 absolute right-0 rounded-r-full'>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                                 </svg>
                             </div>
                         </form>
 
-                        <div className='bg-[#262626] rounded-full w-10 h-10 flex items-center justify-center p-2'>
+                        <div className='bg-[#262626] hidden md:flex rounded-full w-10 h-10  items-center justify-center p-2'>
                             <p><AiFillAudio size={20} /></p>
                         </div>
                     </div>
                 </div>
                 <div className='flex items-center gap-2'>
-                    <div className='bg-[#262626] rounded-full w-10 h-10 flex items-center justify-center p-2'>
+                    <div className='bg-[#262626] rounded-full  w-10 h-10 hidden md:flex items-center justify-center p-2'>
                         <span><RiVideoAddLine size={20} /></span>
                     </div>
-                    <div className='bg-[#262626] rounded-full w-10 h-10 flex items-center justify-center p-2'>
+                    <div className='bg-[#262626] rounded-full w-10 h-10 hidden md:flex items-center justify-center p-2'>
                         <span><IoIosNotificationsOutline size={20} /></span>
                     </div>
                     <div className='bg-[#262626] rounded-full w-10 h-10 flex items-center justify-center p-2'>
