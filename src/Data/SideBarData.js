59ece93bbb231a7cfbc1715d5d56ed156a4cb7b6
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-06 01:20:32 +0530

    sidebar data external fetched

---
+++ src/Data/SideBarData.js
@@ -0,0 +1,109 @@
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { IoIosMenu } from "react-icons/io";
+
+export const SData = [
+    {
+        name: "Home",
+        icon: <IoMdHome size={25} />,
+    },
+    {
+        name: "Shorts",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+    {
+        name: "Subscriptions",
+        icon: <MdOutlineSubscriptions size={25} />,
+    },
+    {
+        name: "History",
+        icon: <IoMdHome size={25} />,
+    }
+]
+
+
+export const SDataMainS1 = [
+    {
+        name: "Home",
+        icon: <IoMdHome size={25} />,
+    },
+    {
+        name: "Shorts",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+    {
+        name: "Subscriptions",
+        icon: <MdOutlineSubscriptions size={25} />,
+    },
+
+]
+
+
+export const SDataMainS2 = [
+    {
+        name: "You",
+        icon: <IoMdHome size={25} />,
+    },
+
+    {
+        name: "History",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+]
+
+
+export const SDataMainS3 = [
+    {
+        name: "Trending",
+        icon: <IoMdHome size={25} />,
+    },
+
+    {
+        name: "Shopping",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "Music",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "Movies",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "Live",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "Gaming",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "News",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+    {
+        name: "Sports",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+    {
+        name: "Courses",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+    {
+        name: "Fashion & Beauty",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+    {
+        name: "Podcasts",
+        icon: <SiYoutubeshorts size={25} />,
+    },
+
+]
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ import { SiYoutubeshorts } from "react-icons/si";
 import { MdOutlineSubscriptions } from "react-icons/md";
 import { IoIosMenu } from "react-icons/io";
 import Image from "next/image";
+import { SData, SDataMainS1, SDataMainS2, SDataMainS3 } from "@/Data/SideBarData";
 const Sidebar = () => {
   const [nav, setNav] = useState(true)
   const showNav = () => {
@@ -13,24 +14,17 @@ const Sidebar = () => {
   // #0f0f0f
   return (
     <>
-      <div className={!nav ? 'min-h-screen w-full p-4 flex flex-col items-center max-w-[100px]  ' : 'hidden'}>
+      <div className={!nav ? 'min-h-screen w-full p-4 hidden md:flex flex-col items-center max-w-[100px]  ' : 'hidden'}>
         <div className='flex flex-col gap-2 w-full '>
 
           <span onClick={showNav} className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><IoIosMenu size={25} /></span>
 
-
-          <span className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><IoMdHome size={25} /> Home</span>
-
-          <span className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><SiYoutubeshorts size={25} /> Shorts</span>
-
-
-          <span className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><MdOutlineSubscriptions size={25} /> Subscriptions</span>
-
-          <span className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><MdOutlineSubscriptions size={25} /> You</span>
-
-
-          <span className='text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center'><MdOutlineSubscriptions size={25} /> History</span>
-
+          {SData.map((item, index) => (
+            <div className="text-xs font-medium hover:bg-slate-500 rounded-xl p-2 flex flex-col gap-2 items-center" key={index}>
+              {item.icon} {/* Render the icon component here */}
+              <p>{item.name}</p>
+            </div>
+          ))}
 
 
 
@@ -39,7 +33,7 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className={nav ? 'relative   h-full  min-w-[230px]  max-w-[250px]  overflow-y-auto    w-full  scroll-smooth' : 'hidden  h-full overflow-y-scroll scroll-smooth flex-col w-full p-2 bg-[#0f0f0f] min-h-screen overflow-hidden max-w-[500px] relative top-0'}>
+      <div className={nav ? 'relative hidden md:block  h-full  min-w-[230px]  max-w-[250px]  overflow-y-auto    w-full  scroll-smooth' : 'hidden  h-full overflow-y-scroll scroll-smooth flex-col w-full p-2 bg-[#0f0f0f] min-h-screen overflow-hidden max-w-[500px] relative top-0'}>
         <div className="flex h-screen flex-col">
 
           <div className="p-4 pt-4 flex items-center gap-5">
@@ -54,55 +48,46 @@ const Sidebar = () => {
 
 
           <div className='flex py-4 flex-col gap-2 w-full border-b border-[#585858c5]'>
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><IoMdHome size={25} /> Home</span>
+  
+            {SDataMainS1.map((item, index) => (
+            <span className="text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center" key={index}>
+              {item.icon} {/* Render the icon component here */}
+              <p>{item.name}</p>
+            </span>
+          ))}
+    
 
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Shorts</span>
+          </div>
 
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><MdOutlineSubscriptions size={25} /> Subscriptions</span>
-
-        </div>
-
-        <div className='flex py-4 flex-col gap-2 w-full border-b border-[#585858c5]'>
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><IoMdHome size={25} /> You</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> History</span>
-        </div>
-
-
-        <div className='flex py-4 flex-col gap-2 w-full  border-[#585858c5]'>
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><IoMdHome size={25} /> Trending</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Shopping</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Music</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Movies</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Live</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Gaming</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> News</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Sports</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Courses</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Fashion & Beauty</span>
-
-          <span className='text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center'><SiYoutubeshorts size={25} /> Podcasts</span>
+          <div className='flex py-4 flex-col gap-2 w-full border-b border-[#585858c5]'>
+          {SDataMainS2.map((item, index) => (
+            <span className="text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center" key={index}>
+              {item.icon} {/* Render the icon component here */}
+              <p>{item.name}</p>
+            </span>
+          ))}
+          </div>
 
 
-        </div>  
+          <div className='flex py-4 flex-col gap-2 w-full  border-[#585858c5]'>
+           
+          {SDataMainS3.map((item, index) => (
+            <span className="text-sm font-semibold hover:bg-slate-500 rounded-xl px-4  py-2 flex gap-5 items-center" key={index}>
+              {item.icon} {/* Render the icon component here */}
+              <p>{item.name}</p>
+            </span>
+          ))}
+
+          </div>
 
         </div>
 
 
 
 
-     
 
-   
+
+
 
       </div>
     </>
