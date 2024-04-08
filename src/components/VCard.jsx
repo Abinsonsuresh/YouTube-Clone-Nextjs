f25141e4d8e6f86ecd926fa9d5bffc014435cb49
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-04-08 20:37:53 +0530

    fix

--- src/components/VCard.jsx
+++ src/components/VCard.jsx
@@ -9,7 +9,7 @@ export const VCard = ({item,  index}) => {
         <div>
             <div className="cursor-pointer p-2 max-h-[390px] max-w-[390px]" >
                 <div className="relative">
-                    <Image className="rounded-lg max-h-[200px] object-cover" height={360} width={480} src={item?.snippet?.thumbnails?.medium?.url} alt="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png" />
+                    <Image className="rounded-lg max-h-[200px] object-cover" height={360} width={480} src={item?.snippet?.thumbnails?.medium?.url} alt="" />
                     <div className="absolute bg-[#1d1d1d] p-1 bottom-1 rounded-xs right-1 font-semibold rounded-md  text-xs">
                         {Math.floor(Math.random() * (59 - 10) + 10)} : {Math.floor(Math.random() * (59 - 10) + 10)}
                     </div>
@@ -17,7 +17,7 @@ export const VCard = ({item,  index}) => {
 
                 {/* width: 480, height: 360  */}
                 <div className="flex gap-4 my-2">
-                    <Image className="rounded-full w-10 h-10" height={10} width={10} src={item?.snippet?.thumbnails?.high?.url} alt="https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png" />
+                    <Image className="rounded-full w-10 h-10" height={10} width={10} src={item?.snippet?.thumbnails?.high?.url} alt="" />
                     <div className="flex flex-col">
                         <p>{item?.snippet?.title}</p>
                         <p className="mt-2 text-gray-500">{item?.snippet?.channelTitle}</p>
